825e198d7170b43a42a5f5842e31d81a947eee6c
Jyuri <jeon1310@gmail.com>
2021-10-04 17:41:42 +0900

    상호평가-다른사람답

--- programmers/src/programmers/Solution07.java
+++ programmers/src/programmers/Solution07.java
@@ -67,7 +67,35 @@ public class Solution07 {
 		}
 		return answer;
 	}
-
+	//다른사람 답
+	public String otherSolution(int[][] scores) {
+        StringBuilder builder = new StringBuilder();
+        for(int i=0; i<scores.length; i++) {
+            int max = 0;
+            int min = 101;
+            int sum = 0;
+            int divide = scores.length;
+            for(int j=0; j<scores.length; j++) {
+                int score = scores[j][i];
+                if(i != j) {
+                    if(score < min) {
+                        min = score;
+                    }
+                    if(score > max) {
+                        max = score;
+                    }
+                }
+                sum += score;
+            }
+            if(scores[i][i] < min || scores[i][i] > max) {
+                sum -= scores[i][i];
+                divide--;
+            }
+            double score = (double) sum / divide;
+            builder.append(score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : score >= 50 ? "D" : "F" );
+        }
+        return builder.toString();
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution07 s7 = new Solution07();
