f099a1c48eecb80c79cc86a24ec4d4eaa904ce69
Jyuri <jeon1310@gmail.com>
2021-10-06 14:45:17 +0900

    (위클리챌린지)직업군 추천하기 풀이방법 계획만

---
+++ programmers/src/programmers/Solution09.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class Solution09 {
+	/*
+	 * 직업군 언어 점수를 정리한 문자열 배열 table, 개발자가 사용하는 언어를 담은 문자열 배열 languages, 언어 선호도를 담은 정수
+	 * 배열 preference가 매개변수로 주어집니다. 개발자가 사용하는 언어의 언어 선호도 x 직업군 언어 점수의 총합이 가장 높은 직업군을
+	 * return 하도록 solution 함수를 완성해주세요. 총합이 같은 직업군이 여러 개일 경우, 이름이 사전 순으로 가장 빠른 직업군을
+	 * return 해주세요.
+	 */
+	public String solution(String[] table, String[] languages, int[] preference) {
+        String answer = "";
+        // 직업군 언어 검색담을 key
+        
+        // 직업군 언어 나누기 -2차원배열
+        // 개발자 사용언어 첫번째부터 검색하기
+        // 직업군 언어 for문 돌리기 i
+        	// 직업군 언어 for문 돌리기 j
+        		//찾는 직업 위에서부터 검색 key제일 위쪽거 가져오기
+        	//점수 key로 가져와서 해당언어선호도랑 곱하기
+        return answer;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
