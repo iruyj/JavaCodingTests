e2a3a7a50998ac970b0f183632243c215cb4ce17
Jyuri <jeon1310@gmail.com>
2021-10-08 15:57:33 +0900

    포켓몬 - Set을 이용한 풀이방법

--- programmers/src/programmers/Solution12.java
+++ programmers/src/programmers/Solution12.java
@@ -15,7 +15,16 @@ public class Solution12 {
 		// 길이랑 비교하기
 		return (answer < cnt) ? answer : cnt;
 	}
-
+	
+	// Set을 이용한 풀이방법
+	public int solution2(int[] nums) {
+        HashSet<Integer> a = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            a.add(nums[i]);
+        }
+        return (nums.length / 2 > a.size()) ? a.size() : nums.length / 2;
+    }
+	
 	public static void main(String[] args) {
 		Solution12 s12 = new Solution12();
 		System.out.println(s12.solution(new int[] { 3,1,2,3 }));
