4c4dc22c0591ab58cfdafd765c3dfc96817d2587
Jyuri <jeon1310@gmail.com>
2021-10-18 23:54:42 +0900

    제곱근 판별하기- 삼항연산자를 이용한 간단한 풀이방법

--- programmers/src/programmers/Solution20.java
+++ programmers/src/programmers/Solution20.java
@@ -1,18 +1,26 @@
 package programmers;
+
 // 제곱근 판별
 /*임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
  * n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, 
  * n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요. 
  * */
 public class Solution20 {
-	// 제곱근 소수점 버리고 구하기 > 제곱인지 확인하기 
+	// 제곱근 소수점 버리고 구하기 > 제곱인지 확인하기
 	public long solution(long n) {
-		long sqrtNum = (long)Math.sqrt(n);
-        if (n/sqrtNum==sqrtNum && n%sqrtNum==0) {
-			return (long) Math.pow(sqrtNum+1, 2);
-        }
-        return -1;
-    }
+		long sqrtNum = (long) Math.sqrt(n);
+		if (n / sqrtNum == sqrtNum && n % sqrtNum == 0) {
+			return (long) Math.pow(sqrtNum + 1, 2);
+		}
+		return -1;
+	}
+
+	// 삼항연산자를 이용한 새로운 풀이방법
+	public long newSolution(long num) {
+		double sqrt = Math.sqrt(num);
+		return (long) ((sqrt % 1) == 0 ? (sqrt + 1) * (sqrt + 1) : -1);
+	}
+
 	public static void main(String[] args) {
 		Solution20 s20 = new Solution20();
 		System.out.println(s20.solution(1));
