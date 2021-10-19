bdb5cbc598f24dd805e7dbb7bbd92f413ffb5a16
Jyuri <jeon1310@gmail.com>
2021-10-19 23:04:16 +0900

    약수의 개수와 덧셈 - 제곱수를 이용한 풀이

--- programmers/src/programmers/Solution21.java
+++ programmers/src/programmers/Solution21.java
@@ -17,4 +17,21 @@ public class Solution21 {
 		}
         return answer;
     }
+	// 제곱수를 이용한 풀이
+	 public int solution2(int left, int right) {
+	        int answer = 0;
+
+	        for (int i=left;i<=right;i++) {
+	            //제곱수인 경우 약수의 개수가 홀수
+	            if (i % Math.sqrt(i) == 0) {
+	                answer -= i;
+	            }
+	            //제곱수가 아닌 경우 약수의 개수가 짝수
+	            else {
+	                answer += i;
+	            }
+	        }
+
+	        return answer;
+	    }
 }
